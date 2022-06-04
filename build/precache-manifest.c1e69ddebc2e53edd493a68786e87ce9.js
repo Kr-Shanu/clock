e39645e3c9136dc4e28ea5d46a668846e068f8c7
Kumar Shanu <kumar.shanu2020@vitbhopal.ac.in>
2022-06-04 12:10:43 +0530

    Trying to publish the application on github pages

---
+++ build/precache-manifest.c1e69ddebc2e53edd493a68786e87ce9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b61ceb78e88808643761a5a071affe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1a0636bc930e7b7e7c7",
+    "url": "/static/js/2.db7586e6.chunk.js"
+  },
+  {
+    "revision": "ed3934126f2acc4a6ffa",
+    "url": "/static/js/main.60f2fe1b.chunk.js"
+  },
+  {
+    "revision": "b070c057ff7bda948ba3",
+    "url": "/static/js/runtime-main.8bdb7793.js"
+  }
+]);
